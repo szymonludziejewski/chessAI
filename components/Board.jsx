473a31748ddfffa750f9e6659d2181bc5c91bd3d
Szymon <ludziejewskiszymon@gmail.com>
2023-10-06 22:43:27 +0200

    add some configuration, add new compnent

--- components/Board.jsx
+++ components/Board.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 import Field from './Field'
 
 export default function Board() {
-  return (
-    <Field></Field>
-  )
+    const columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    const rows = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+    return (
+        <tbody>
+            {columns.map(column =>
+            <tr>
+                {rows.map(row =>
+                    <td>
+                        <Field cordinates={`${column}${row}`}></Field>
+                    </td>
+                )}
+            </tr>
+            )}
+        </tbody>
+    )
 }
---
+++ components/Figure.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Figure() {
+    
+    return (
+        <div>Figure</div>
+    )
+}
