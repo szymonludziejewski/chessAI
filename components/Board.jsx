d3a541a5f82cc34c4bdab9e7b037a9249ffdbe1e
Szymon <ludziejewskiszymon@gmail.com>
2023-10-06 21:18:43 +0200

    begin with simple components

---
+++ components/Board.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Field from './Field'
+
+export default function Board() {
+  return (
+    <Field></Field>
+  )
+}
---
+++ components/Field.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Field() {
+    const color = '';
+    const cordinates = '';
+    return (
+        <div>Field</div>
+    )
+}
